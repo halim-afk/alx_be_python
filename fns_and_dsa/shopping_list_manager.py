9e79b2bb6b8ac99c335cc67a8d9d01203aee4dc3
halim-afk <a.alouah0243@uca.ac.ma>
2025-06-06 20:33:40 +0100

    add list_shoping file

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,38 @@
+
+shopping_list = []
+
+def display_items():
+    for item in shopping_list:
+        print(item)
+
+def add_to_list(product):
+    shopping_list.append(product)
+    display_items()
+
+status = input("Do you want to add, remove, or display? ")
+
+if status == "add":
+    product = input("Enter the name of the product: ")
+    add_to_list(product)
+
+elif status == "remove":
+    product = input("Enter the name of the product to remove: ")
+    if product in shopping_list:
+        shopping_list.remove(product)
+    else:
+        print("Product not found.")
+    display_items()
+
+elif status == "display":
+    display_items()
+
+else:
+    print("Unknown request")
+
+
+
+
+
+
+
+
