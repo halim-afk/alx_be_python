24f3336b72f9dd329eb3b92a25304465b0f46c81
halim-afk <a.alouah0243@uca.ac.ma>
2025-06-06 15:33:53 +0100

    fix some bugs

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -5,7 +5,12 @@ def perform_operation(num1, num2, operation):
         return num1 - num2
     elif operation == "multiply":
         return num1 * num2
-    else:
+    elif operation == "divide":
+        if num2 == 0: 
+            return "Error: Cannot divide by zero"
         return num1 / num2
+    else:
+        return "Error: Invalid operation"
+
 
 
