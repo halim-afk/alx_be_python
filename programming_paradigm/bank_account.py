d0d20bfd724a0debc4124b862a85dc0fb20b1777
halim-afk <a.alouah0243@uca.ac.ma>
2025-06-13 12:27:41 +0100

    debug

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -4,6 +4,11 @@ class BankAccount:
     def __init__(self, initial_balance=0):
         # تعريف الرصيد الابتدائي للحساب البنكي
         self.account_balance = initial_balance
+    
+    def display_balance(self):
+        # طباعة الرصيد الحالي بصيغة واضحة
+        print(f"Current Balance: ${self.account_balance}")
+
 
     def deposit(self, amount):
         # إيداع مبلغ في الحساب
@@ -18,6 +23,4 @@ class BankAccount:
             # إذا لم يكن هناك رصيد كافٍ، لا يتم السحب
             return False
 
-    def display_balance(self):
-        # طباعة الرصيد الحالي بصيغة واضحة
-        print(f"Current Balance: ${self.account_balance}")
+    
