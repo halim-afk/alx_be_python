51465ab2a5714488d1b0d0486532e264433d5ea7
halim-afk <a.alouah0243@uca.ac.ma>
2025-06-13 11:31:37 +0100

    initial commit

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,4 +13,4 @@ class BankAccount:
          print(f"Insufficient funds.")
 
     def display_balance(self):
-        print(f" Current Balance: $ {self.account_balance}")
+        print(f"Current Balance: ${self.account_balance}")
