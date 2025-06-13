167f9e932243a469fa51ef3a879fe7463f8d11ec
halim-afk <a.alouah0243@uca.ac.ma>
2025-06-13 11:50:03 +0100

    fix bugs

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -7,7 +7,10 @@ class BankAccount:
 
     def withdraw(self ,amount):
        self.account_balance -= amount
-       
+       if self.account_balance >= amount:
+         print(f"Withdraw: ${self.account_balance}")
+       else:
+         print(f"Insufficient funds.")
 
     def display_balance(self):
-        print(f"Current Balance:${self.account_balance}")
+        print(f"Current Balance: $ {self.account_balance}")
