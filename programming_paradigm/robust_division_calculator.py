73a64be3fa6ce9c4db75d8d521b528432fc6741f
halim-afk <a.alouah0243@uca.ac.ma>
2025-06-13 16:49:05 +0100

    initial commit

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,17 +3,13 @@ def safe_divide(numerator, denominator):
         # محاولة تنفيذ القسمة
         result = float(numerator) / float(denominator) 
         print(f"The result of the division is {result}")
-        return "Successful division."
     except ZeroDivisionError:
         # معالجة خطأ القسمة على صفر
         print("Error: Cannot divide by zero.")
-        return "Division failed."
 
     except ValueError:
         # معالجة خطأ إدخال غير رقمي
         print("Error: Please enter numeric values only.")
-        return "Division failed."
 
 
-    except:
-           return "the operation is finish"
+    
