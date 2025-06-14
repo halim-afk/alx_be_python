3526c4c4ed7deb73dc3fbba2824519c8932c7382
halim-afk <a.alouah0243@uca.ac.ma>
2025-06-14 10:26:10 +0100

    fix bugs

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -8,7 +8,7 @@ class TestSimpleCalculator(unittest.TestCase):
         """تهيئة كائن الآلة الحاسبة قبل كل اختبار"""
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, -1), -2)
         self.assertEqual(self.calc.add(-1, 1), 0)
